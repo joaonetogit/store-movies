24a5936ff40ef332533f4f25a46e9ae8ca3d7208
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2024-06-10 13:52:40 -0300

    fix: error on build

--- src/utils/convertCoin.ts
+++ src/utils/convertCoin.ts
@@ -1,4 +1,4 @@
-import { HasSymbol } from "@/types/utils/convertCoin";
+import { HasSymbol } from "@/types/utils/ConvertCoin";
 
 export default function ConvertCoin(value: number, hasSymbol: HasSymbol): string {
   const valueFormatted = new Intl.NumberFormat('en-US', {
