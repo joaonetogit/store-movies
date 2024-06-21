a54086e23e4c32967f5727c73eefd5ad456bb184
João Domingos Pinheiro Neto <56659658+joaonetogit@users.noreply.github.com>
2024-06-21 15:16:37 -0300

    build: update chunks in build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,27 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) return 'vendor';
+          if (id.includes('src/__tests__')) return 'tests';
+          if (id.includes('src/api')) return 'api';
+          if (id.includes('src/components')) return 'components';
+          if (id.includes('src/constants')) return 'constants';
+          if (id.includes('src/context')) return 'context';
+          if (id.includes('src/functions')) return 'functions';
+          if (id.includes('src/hooks')) return 'hooks';
+          if (id.includes('src/layouts')) return 'layouts';
+          if (id.includes('src/pages')) return 'pages';
+          if (id.includes('src/types')) return 'types';
+          if (id.includes('src/utils')) return 'utils';
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
   plugins: [react()],
   server: {
     open: true,
